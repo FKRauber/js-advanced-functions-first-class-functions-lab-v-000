9fed4a911e013eb3a97c16e379d8c77777247de5
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-27 02:48:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
 
-const returnFirstTwoDrivers = function (array) { return array.slice(0, 2); };
+const returnFirstTwoDrivers = function (drivers) { 
+  return array.slice(0, 2); 
+};
 
 function returnLastTwoDrivers(){
 
