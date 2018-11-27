e3370af517b08c6d0b68835e0accd9f50b845599
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-27 02:39:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 // Code your solution in this file!
 
 function returnFirstTwoDrivers(){
-  
+
 }
 
 function returnLastTwoDrivers(){
-  
+
 }
 
 function selectingDrivers(){
-  
+
 }
 
 function createFareMultiplier(){
-  
+
 }
 
 function fareDoubler(){
-  
+
 }
 
 function fareTripler(){
-  
+
 }
 
-function fetchSpecifiedDrivers(){
-  
+function fetchSpecifiedDrivers(arrayOfDrivers, function){
+
 }
